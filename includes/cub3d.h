fb0799957339e3990bef393e61838921f035b556
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-22 09:10:50 -0300

    style: reorganize header includes and update author

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,28 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 12:02:46 by jlacerda          #+#    #+#             */
-/*   Updated: 2025/08/14 01:11:29 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/08/22 05:21:02 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <math.h>
+# include "constants.h"
+# include "libft.h"
+# include "parser.h"
+# include "structs.h"
+# include <MLX42/MLX42.h>
 # include <fcntl.h>
+# include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
 # include <sys/time.h>
-
-# include "libft.h"
-# include <MLX42/MLX42.h>
-
-# include "structs.h"
-# include "constants.h"
+# include <unistd.h>
 
 size_t	mm_nodes_alloc_length(void);
 void	mm_garbage_collector(void);
