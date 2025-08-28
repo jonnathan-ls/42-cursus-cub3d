45200b0efac53f6fefdb681f7fb28a38e1baae50
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-28 20:26:54 -0300

    fix(parser): add error handling for write syscalls in parser_error

--- sources/parser/parser_errors.c
+++ sources/parser/parser_errors.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 05:23:48 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/22 08:39:46 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/08/28 20:25:12 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,18 @@
 
 int	parser_error(const char *msg)
 {
+	ssize_t	ret;
+
 	if (!msg)
 		return (-1);
-	write(2, "Error\n", 6);
-	write(2, msg, ft_strlen(msg));
-	write(2, "\n", 1);
+	ret = write(2, "Error\n", 6);
+	if (ret == -1)
+		return (-1);
+	ret = write(2, msg, ft_strlen(msg));
+	if (ret == -1)
+		return (-1);
+	ret = write(2, "\n", 1);
+	if (ret == -1)
+		return (-1);
 	return (-1);
 }
