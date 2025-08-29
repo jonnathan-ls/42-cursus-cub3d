525ebff9ffc2a8caa66a31c710e6a1234b059b6d
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-29 07:23:56 -0300

    feat(raycast): add distance calculations for rendering

---
+++ sources/engine/ray_distance.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray_distance.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 20:53:28 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/28 21:40:21 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "constants.h"
+#include "engine.h"
+#include "raycast.h"
+#include <math.h>
+
+void	ft_calculate_distances(t_ray *ray, t_player *pl)
+{
+	if (ray->side == 0)
+		ray->perp_dist = (ray->map_x - pl->pos_x + (1 - ray->step_x) / 2.0)
+			/ ray->ray_dir_x;
+	else
+		ray->perp_dist = (ray->map_y - pl->pos_y + (1 - ray->step_y) / 2.0)
+			/ ray->ray_dir_y;
+	if (ray->perp_dist < DIST_EPS)
+		ray->perp_dist = DIST_EPS;
+}
+
+void	ft_apply_fish_eye_correction(t_ray *ray, t_player *pl)
+{
+	(void)pl;
+	(void)ray;
+}
