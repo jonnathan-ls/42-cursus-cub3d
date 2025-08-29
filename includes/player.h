af879938238b12c1c14324fd487509a23a3ee3d8
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-29 07:23:09 -0300

    feat(player): define player state and movement interface

---
+++ includes/player.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 20:48:52 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/29 07:11:56 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PLAYER_H
+# define PLAYER_H
+
+# include <stddef.h>
+
+struct	s_engine;
+
+typedef struct s_player
+{
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	move_speed;
+	double	rot_speed;
+}			t_player;
+
+void		ft_player_move(struct s_engine *eng);
+void		ft_player_rotate(struct s_engine *eng, double rot);
+
+#endif
