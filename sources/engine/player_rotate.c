4811d10af92a9b84f9a58303f9fa22cae6571f9b
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-29 07:23:56 -0300

    feat(player): add rotation mechanics with matrix transforms

---
+++ sources/engine/player_rotate.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_rotate.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 20:53:13 by peda-cos          #+#    #+#             */
+/*   Updated: 2025/08/28 21:40:40 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "engine.h"
+#include "player.h"
+#include <MLX42/MLX42.h>
+#include <math.h>
+
+static void	apply_rot(t_player *p, double rot)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = p->dir_x;
+	old_plane_x = p->plane_x;
+	p->dir_x = p->dir_x * cos(rot) - p->dir_y * sin(rot);
+	p->dir_y = old_dir_x * sin(rot) + p->dir_y * cos(rot);
+	p->plane_x = p->plane_x * cos(rot) - p->plane_y * sin(rot);
+	p->plane_y = old_plane_x * sin(rot) + p->plane_y * cos(rot);
+}
+
+void	ft_player_rotate(t_engine *eng, double rot)
+{
+	if (!eng)
+		return ;
+	apply_rot(&eng->player, rot);
+}
