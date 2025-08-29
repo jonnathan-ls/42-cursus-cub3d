3998f4272c1208a8b754969b7a1258590edd17b4
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-08-29 07:21:34 -0300

    feat: add FOV, movement and distance constants

--- includes/constants.h
+++ includes/constants.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   constants.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 12:02:46 by jlacerda          #+#    #+#             */
-/*   Updated: 2025/08/10 17:55:31 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/08/28 21:40:24 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,12 @@
 # define WIN_HEIGHT 600
 
 # define WHITE 0xFFFFFFFF
+
+# define FOV_DEG 60
+# define PLANE_FACTOR 0.66
+# define MOVE_SPEED 0.08
+# define ROT_SPEED 0.05
+
+# define DIST_EPS 0.0001
 
 #endif
