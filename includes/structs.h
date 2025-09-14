0fc870cf041340c463a199e000c0ae04a8674a67
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-09-14 18:54:05 -0300

    refactor(structs): add t_config and include config in t_cub3d

--- includes/structs.h
+++ includes/structs.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   structs.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 12:02:46 by jlacerda          #+#    #+#             */
-/*   Updated: 2025/08/10 18:28:02 by jlacerda         ###   ########.fr       */
+/*   Updated: 2025/09/14 18:16:27 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 
 # include <MLX42/MLX42.h>
 
+typedef struct s_config	t_config;
+
 typedef struct s_mlx42
 {
-	mlx_t		*instance;
-	mlx_image_t	*image;
-}	t_mlx42;
+	mlx_t				*instance;
+	mlx_image_t			*image;
+}						t_mlx42;
 
 typedef struct s_cub3d
 {
-	t_mlx42	mlx;
-}	t_cub3d;
+	t_mlx42				mlx;
+	t_config			config;
+}						t_cub3d;
 
 #endif
