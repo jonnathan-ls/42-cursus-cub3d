51e63d4e166571839ab741397dba1492ffbbad66
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-09-14 19:12:52 -0300

    feat(engine): make rotation frame-rate independent using delta_time

--- sources/engine/engine_loop.c
+++ sources/engine/engine_loop.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 20:57:43 by peda-cos          #+#    #+#             */
-/*   Updated: 2025/08/28 21:40:31 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/09/14 19:06:25 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@
 static void	frame_hook(void *param)
 {
 	t_engine	*eng;
+	double		delta_time;
 
 	eng = (t_engine *)param;
 	if (mlx_is_key_down(eng->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(eng->mlx);
+	delta_time = eng->mlx->delta_time;
 	ft_player_move(eng);
 	if (mlx_is_key_down(eng->mlx, MLX_KEY_LEFT))
-		ft_player_rotate(eng, -eng->player.rot_speed);
+		ft_player_rotate(eng, -eng->player.rot_speed * delta_time * 60.0);
 	if (mlx_is_key_down(eng->mlx, MLX_KEY_RIGHT))
-		ft_player_rotate(eng, eng->player.rot_speed);
+		ft_player_rotate(eng, eng->player.rot_speed * delta_time * 60.0);
 	ft_cast_all_rays(eng);
-	ft_render_walls(eng);
-	ft_update_image_buffer(eng);
 }
 
 void	ft_engine_loop(t_engine *eng)
