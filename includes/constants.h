851637861288232788b031cf4f4ff488c461028f
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-09-14 21:07:57 -0300

    feat(constants): add mouse sensitivity and tweak speeds

--- includes/constants.h
+++ includes/constants.h
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 12:02:46 by jlacerda          #+#    #+#             */
-/*   Updated: 2025/08/28 21:40:24 by peda-cos         ###   ########.fr       */
+/*   Updated: 2025/09/14 19:44:49 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,9 @@
 
 # define FOV_DEG 60
 # define PLANE_FACTOR 0.66
-# define MOVE_SPEED 0.08
-# define ROT_SPEED 0.05
+# define MOVE_SPEED 0.1
+# define ROT_SPEED 0.07
+# define MOUSE_SENSITIVITY 0.002
 
 # define DIST_EPS 0.0001
 
